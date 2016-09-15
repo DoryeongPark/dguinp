1f107788ee0c60e257f566a305e5f872041ba957
Doryeong Park <zzada0520@gmail.com>
2016-09-15 13:57:15 +0900

    Section - Home updated

--- blog/urls.py
+++ blog/urls.py
@@ -2,12 +2,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.home, name='home'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 
-    # Practice code
+    # C++
+    # url(r'^cpp/$',views.cpp_home, name='cpp_home'),
+
+    # Internet Programming Assignment
     url(r'^exercise/(?P<num>[0-9]+)/$', views.exercise, name="exercise"),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import get_object_or_404, redirect, render
 from .models import Post
 from .forms import PostForm
 
-#Project Codes
+def home(request):
+    return render(request, 'blog/home.html')
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
