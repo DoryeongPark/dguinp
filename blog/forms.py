bcada3f7bf58cbdbe02574f35b1478479c46aafb
Doryeong Park <zzada0520@gmail.com>
2016-10-03 02:51:35 +0900

    Login form updated

--- blog/forms.py
+++ blog/forms.py
@@ -5,7 +5,7 @@ from django_summernote import fields as summer_fields
 
 class PostForm(forms.ModelForm):
     detail_error = {'required':(u'상세정보를 입력해주세요'),}
-    #detail = summer_fields.SummernoteTextFormField(error_messages=detail_error)
+
     class Meta:
         model = Detail
         fields = ('name','issue','class_detail')
